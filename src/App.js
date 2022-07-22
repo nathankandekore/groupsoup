1cf865b3be05a922544ed8c8e5db104a3f00919b
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-07-22 10:26:14 +0100

    timer usingusestate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Logo from "./logo.svg";
+
 // import Logo2 from "./logo2.svg";
-import Timer from "./timer";
+// import Timer from "./timer";
 
 function App() {
+  // let minutes = 20;
+  // let seconds = 0;
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(20);
+
+  function startTimer(event) {
+    if (minutes > 0) {
+      setMinutes((minutes) => minutes - 1);
+      setSeconds((seconds) => (seconds = 59));
+    }
+    setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
+    }, 1000);
+  }
+
   return (
     <div className="App">
       <div className="titleAndLogo">
@@ -13,25 +31,35 @@ function App() {
       </div>
 
       <div className="timer">
-        <Timer />
+        <div className="clock-container">
+          {minutes < 10 ? `0${minutes}` : minutes}:
+          {seconds < 10 ? `0${seconds}` : seconds}
+        </div>
+        <button className="timer-btn" onClick={() => startTimer()}>
+          start
+        </button>
       </div>
 
       <div className="textbox-container">
         <textarea
           className="case-presentation textbox-sizing"
           placeholder="case presentation"
+          spellCheck="false"
         ></textarea>
         <textarea
           className="clarifying-questions textbox-sizing"
           placeholder="clarifying questions"
+          spellCheck="false"
         ></textarea>
         <textarea
           className="hypothesizing textbox-sizing"
           placeholder="hypothesizing"
+          spellCheck="false"
         ></textarea>
         <textarea
           className="reflection textbox-sizing"
           placeholder="reflection"
+          spellCheck="false"
         ></textarea>
       </div>
       <button>finish</button>
--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,19 @@
 import React from "react";
+// import { useState } from "react";
 
-export default function timer() {
+export default function Timer(props) {
+  let minutes = 20;
+  let seconds = 0;
+
   return (
     <div className="timer">
-      <div className="clock-container">20:00</div>
-      <button className="timer-btn">start</button>
+      <div className="clock-container">
+        {minutes < 10 ? `0${minutes}` : minutes}:
+        {seconds < 10 ? `0${seconds}` : seconds}
+      </div>
+      <button className="timer-btn" onClick={() => console.log("clicked")}>
+        start
+      </button>
     </div>
   );
 }
